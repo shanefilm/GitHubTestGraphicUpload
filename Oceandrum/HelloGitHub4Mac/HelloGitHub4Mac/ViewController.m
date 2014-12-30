3eed80fc594139aa128eb934ec1070047c58a95c
Shane Harris <shanefilm@gmail.com>
2014-12-30 09:59:10 -0500

    Hello World project
    testing the graphic upload

---
+++ Oceandrum/HelloGitHub4Mac/HelloGitHub4Mac/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloGitHub4Mac
+//
+//  Created by Shane Harris on 12/29/14.
+//  Copyright (c) 2014 Oceandrum Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Oceandrum/HelloGitHub4Mac/HelloGitHub4MacTests/HelloGitHub4MacTests.m
@@ -0,0 +1,40 @@
+//
+//  HelloGitHub4MacTests.m
+//  HelloGitHub4MacTests
+//
+//  Created by Shane Harris on 12/29/14.
+//  Copyright (c) 2014 Oceandrum Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface HelloGitHub4MacTests : XCTestCase
+
+@end
+
+@implementation HelloGitHub4MacTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
